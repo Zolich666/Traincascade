ee4a29c21fa5a570b4965f6099b7fe9b7e9ecd1c
Licheng Zong <Licheng.Zong@outlook.com>
2019-02-14 10:42:14 +0800

    add sprintf

--- main.cpp
+++ main.cpp
@@ -23,21 +23,24 @@ int main()
 	Mat CopyTest;
 	vector<Rect>goals_all,goals_final;
 	CascadeClassifier cascade;
-	cascade.load("F:\\Adaboost\\0123MLTest\\cascade1.xml");
+	cascade.load("F:\\Adaboost\\0214MLTest\\cascade.xml");
 	IplImage* pImg;
 	int Scores[20];
-	ofstream out_txt("output.txt");
+	ofstream out_txt("d.txt");
 	out_txt.close();
-	for (k = 1; k < 140; k++)
+	for (k = 1; k < 42; k++)
 	{
-		TestImage = imread("F:\\Adaboost\\Test Set\\960_540\\DayTest(" + to_string(k) + ").jpg");
+		char indir[50],outdir[50];
+		sprintf(indir, "F:\\Adaboost\\0214MLTest\\test\\test_%03d.jpg", k);
+		sprintf(outdir, "test/test_%03d", k);
+		TestImage = imread(indir);
 		CopyTest = TestImage.clone();
 
-		ofstream out_txt("output.txt", ios::app);
+		ofstream out_txt("d.txt", ios::app);
 		cascade.detectMultiScale(CopyTest, goals_all, 1.2, 0, 0 | CASCADE_SCALE_IMAGE);//识别图，所有框存入goals_all
 		cascade.detectMultiScale(CopyTest, goals_final, 1.2, CombNum, 0 | CASCADE_SCALE_IMAGE);//识别图，存入容器goals_final，每次矩形框放大倍数，几个矩形框合并
-		cout << "DayTest(" << k << ")" << endl << goals_final.size() << endl;
-		out_txt << "test/DayTest(" << k <<")" << endl << goals_final.size() << endl;
+		cout << outdir << endl << goals_final.size() << endl;
+		out_txt << outdir << endl << goals_final.size() << endl;
 		out_txt.close();
 		//if (goals_final.size() == 0) break;
 		//goals_final.x   .y    .width    .height
@@ -46,13 +49,13 @@ int main()
 		{
 			rectangle(CopyTest, goals_final[i], Scalar(0, 255, 0), 2);//测试图，goals_final[i]所含的矩形框，颜色，线宽
 			cout << goals_final[i].x << " " << goals_final[i].y << " " << goals_final[i].width << " " << goals_final[i].height << " " << Scores[i] << endl;
-			ofstream out_txt("output.txt", ios::app);
+			ofstream out_txt("d.txt", ios::app);
 			out_txt << goals_final[i].x << " " << goals_final[i].y << " " << goals_final[i].width << " " << goals_final[i].height << " " << Scores[i] << endl;
 			out_txt.close();
 		}
-		imshow("DayTest " + to_string(k), CopyTest);
+		imshow(outdir, CopyTest);
 		waitKey(1000);
-		destroyWindow("DayTest " + to_string(k));
+		destroyWindow(outdir);
 	}
 
 
